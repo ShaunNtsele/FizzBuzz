e0875b7f3ea24227a830ed6daf9fc8b2bb2602d3
Linda <Linda@LAPTOP-R1KOF62A>
2022-10-21 14:12:22 +0200

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a number: ");
+            int iNumber;
+            bool isTrue = int.TryParse(Console.ReadLine(),out iNumber);
+            if (isTrue)
+            {
+                if (iNumber % 3 == 0 && iNumber % 5 != 0)
+                    Console.WriteLine("Fizz");
+                else if (iNumber % 5 == 0 && iNumber % 3 != 0)
+                    Console.WriteLine("Buzz");
+                else if (iNumber % 3 == 0 && iNumber % 5 == 0)
+                    Console.WriteLine("Fizzbuzz");
+                else
+                    for (int i = 0; i < iNumber; i++)
+                    {
+                        Console.WriteLine(i + 1);
+                    }
+            }
+            else
+            {
+                Console.WriteLine("Invalid interger value!");
+                Main(args);
+            }
+              
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
